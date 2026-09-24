b5775f9f4f85c46a91f06d70004c9ab3156c88c0
kittiphoom-jdai <66050035@kmitl.ac.th>
2026-09-24 15:32:35 +0700

    Complete cell05 exercises

---
+++ cell05/ex08/aff_rev_params.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+
+params = sys.argv[1:]
+
+if len(params) < 2:
+    print("none")
+else:
+    for param in reversed(params):
+        print(param)
---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) != 2:
+    print("none")
+else:
+    answer = input("What was the parameter? ")
+
+    if answer == sys.argv[1]:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
